7d65e53055ae024fdd532b7b3841ee3b54b00abf
mhavrylova <havrylova.maryna@airslate.com>
2023-10-23 15:29:48 +0300

    topic - functions

--- part1-java-script-fundamentals/chapter-5/functions/tasks/task2.js
+++ part1-java-script-fundamentals/chapter-5/functions/tasks/task2.js
@@ -1,1 +1,7 @@
+function min(a, b) {
+    return (a < b) ? a : b;
+}
 
+console.log(min(1, 2));
+console.log(min(5, 2));
+console.log(min(7, 7));
--- part1-java-script-fundamentals/chapter-5/functions/tasks/task3.js
+++ part1-java-script-fundamentals/chapter-5/functions/tasks/task3.js
@@ -1,1 +1,10 @@
+function pow(x,n) {
+    x = +prompt('enter first number', '3');
+    n = +prompt('enter second number', '2');
+    if (n > 0 && n % 1 === 0) {
+        return x ** n;
+    }
+    return 'enter correct second number';
+}
 
+alert(pow());
