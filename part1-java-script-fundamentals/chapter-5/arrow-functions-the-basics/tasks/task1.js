9f715f1b3f2f1fb6020520ce9d39975191514688
mhavrylova <havrylova.maryna@airslate.com>
2023-10-24 14:27:22 +0300

    topic - loops while, for

--- part1-java-script-fundamentals/chapter-5/arrow-functions-the-basics/tasks/task1.js
+++ part1-java-script-fundamentals/chapter-5/arrow-functions-the-basics/tasks/task1.js
@@ -1,10 +1,25 @@
-let ask = (question, yes, no) => {
+const ask = (question, yes, no) => {
     if (confirm(question)) yes()
     else no();
 }
 
 ask(
-    "Вы согласны?",
-    () => alert("Вы согласились."),
-    () => alert("Вы отменили выполнение")
+    "Are you agree?",
+    () => alert("You are agree"),
+    () => alert("You canceled the action")
 );
+
+const ask2 = (question, agree, disagree) => confirm(question) ? agree() : disagree()
+
+const ask3 = (question, agree, disagree) => {
+    const decision = confirm(question) ? agree : disagree;
+    decision();
+}
+
+const ask4 = (question, agree, disagree) => {
+    if(confirm(question)) {
+        agree()
+        return
+    }
+    disagree();
+}
