a5bc74047323f2e3ee881d568eb5b131d227561b
mhavrylova <havrylova.maryna@airslate.com>
2023-11-09 00:54:35 +0200

    homework - chapter5

---
+++ part1-java-script-fundamentals/chapter-5/0-homework/task3.js
@@ -0,0 +1,10 @@
+import random from "./random.js";
+
+console.log(random(0, 5));
+console.log(random(-10, 0));
+console.log(random(-20, 20));
+console.log(random('20', 'f'));
+console.log(random(20));
+console.log(random(false));
+console.log(random(false, null));
+console.log(random(20, 2));
