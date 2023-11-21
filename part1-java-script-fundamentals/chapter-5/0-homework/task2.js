938134cb5bccffd24bfe77abcdf0a214095c6039
mhavrylova <havrylova.maryna@airslate.com>
2023-11-21 20:37:08 +0200

    homework - chapter5 - fixes after review

--- part1-java-script-fundamentals/chapter-5/0-homework/task2.js
+++ part1-java-script-fundamentals/chapter-5/0-homework/task2.js
@@ -1,9 +1,9 @@
 import isPalindrome from "./isPalindrome2.js";
 
-console.log(isPalindrome(' a roza upala na lapu azora'));
-console.log(isPalindrome('ArozaupalanalApuazOra'));
-console.log(isPalindrome('ama'));
-console.log(isPalindrome('asdsa'));
-console.log(isPalindrome('assaa'));
-console.log(isPalindrome(1234));
-console.log(isPalindrome('123321'));
+console.log('" a roza upala na lapu azora"', isPalindrome(' a roza upala na lapu azora'));
+console.log('"ArozaupalanalApuazOra"', isPalindrome('ArozaupalanalApuazOra'));
+console.log('"ama"', isPalindrome('ama'));
+console.log('"asdsa"', isPalindrome('asdsa'));
+console.log('"assaa"', isPalindrome('assaa'));
+console.log(1234, isPalindrome(1234));
+console.log('"123321"', isPalindrome('123321'));
