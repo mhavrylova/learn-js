87b1463d8917c72d12cd1d13e255822e1158fbf6
mhavrylova <havrylova.maryna@airslate.com>
2023-11-21 22:54:41 +0200

    homework - chapter5 - fixes after review

--- part1-java-script-fundamentals/chapter-5/0-homework/isPalindrome.js
+++ part1-java-script-fundamentals/chapter-5/0-homework/isPalindrome.js
@@ -1,9 +1,13 @@
 import reverseString from "./reverseString.js";
 export default function isPalindrome(str) {
     if (reverseString(str)) {
-        let str1 = str.toLowerCase().split(' ').join('');
-        let str2 = reverseString(str).toLowerCase().split(' ').join('');
-        return str1 === str2;
+        const baseString = formatString(str);
+        const reversedString = formatString(reverseString(str));
+        return baseString === reversedString;
     }
     return 'invalid format';
+}
+
+function formatString(str) {
+    return str.toLowerCase().split(' ').join('');
 }
