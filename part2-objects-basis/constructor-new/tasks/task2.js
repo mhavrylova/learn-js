4fce27e56622b1260156a4f950a1d07ca0ac9d48
mhavrylova <havrylova.maryna@airslate.com>
2024-02-05 12:06:49 +0200

    chapter objects, topic - constructor

---
+++ part2-objects-basis/constructor-new/tasks/task2.js
@@ -0,0 +1,12 @@
+function Accumulator(startingValue) {
+    this.value = startingValue
+    this.read = function() {
+        this.value =  this.value + +prompt('enter number');
+    }
+}
+
+let accumulator = new Accumulator(1);
+
+accumulator.read(); // додає введене користувачем значення
+accumulator.read(); // додає введене користувачем значення
+console.log(accumulator.value); // показує суму цих значень
