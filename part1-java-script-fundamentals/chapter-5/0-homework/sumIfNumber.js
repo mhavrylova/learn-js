224c14160f0b521a5b992b927eb92d4f5b332b6e
mhavrylova <havrylova.maryna@airslate.com>
2024-02-07 11:32:59 +0200

    chapter objects, topic - objects to primitives

--- part1-java-script-fundamentals/chapter-5/0-homework/sumIfNumber.js
+++ part1-java-script-fundamentals/chapter-5/0-homework/sumIfNumber.js
@@ -6,9 +6,12 @@ export function sumIfNumber() {
     }
     return sum;
 }
-export const sumIfNumberArrow = () => {
+
+// the arguments object is not accessible in arrow functions. use the rest parameter syntax (...args) to collect all arguments
+
+export const sumIfNumberArrow = (...args) => {
     let sum = 0;
-    for (let item of arguments) {
+    for (let item of args) {
         if (typeof item !== 'number') continue;
         sum += item;
     }
